c24f7a6f3e7e532ab9926d3304ffc446edf9b186
François Déchelle <fdechelle@teclib.com>
2017-01-27 13:58:22 +0100

    Add icons Change setup.py to install them

--- python/setup.py
+++ python/setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
 import os
 from setuptools import setup
@@ -21,7 +22,15 @@ setup(
         "Topic :: Utilities",
         "License :: OSI Approved :: GPLv3 License",
     ],
-    packages=['armadito'],
-    scripts =['bin/indicator-armadito'],
+    packages = ['armadito'],
+    scripts = ['bin/indicator-armadito'],
+    data_files = [('share/icons/hicolor/scalable/apps', ['icons/scalable/indicator-armadito-dark.svg', 'icons/scalable/indicator-armadito.svg'])]
 )
 
+#
+#
+# reminder: for user local installation
+# ./setup.py install --user
+# ./setup.py install_data --install-dir=/home/fdechelle/.local
+#
+#
