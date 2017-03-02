cbe3545da7855089d252cdc66b4f6100b9a2e394
François Déchelle <fdechelle@teclib.com>
2017-03-02 18:15:40 +0100

    Fix make install

--- python/setup.py
+++ python/setup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+#
+# to combine autotools and setuptools, see:
+# https://blog.kevin-brown.com/programming/2014/09/24/combining-autotools-and-setuptools.html
 
 import os
 from setuptools import setup
