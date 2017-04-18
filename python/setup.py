46391c170dc31f9e2addcc7564051000292ab202
François Déchelle <fdechelle@teclib.com>
2017-04-18 15:12:37 +0200

    Add i18n installation (in progress)

--- python/setup.py
+++ python/setup.py
@@ -12,36 +12,11 @@ from setuptools import setup
 from setuptools.command.install import install as _install
 #from babel.messages import frontend as babel
 
-def do_install_file(file, install_dir):
-    print("Installing %s in %s..." % (file, install_dir))
-    try:
-        os.makedirs(install_dir, mode = 0o755, exist_ok = True)
-    except OSError:
-        print("WARNING: cannot create directory %s" % install_dir)
-        return
-    try:
-        shutil.copy(file, install_dir)
-    except IOError:
-        print("WARNING: cannot copy file %s" % (file,))
-    mode = stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR | stat.S_IRGRP | stat.S_IRGRP | stat.S_IXGRP | stat.S_IROTH | stat.S_IXOTH
-    try:
-        base_file = os.path.basename(file)
-        os.chmod(install_dir + '/' + base_file, mode)
-    except IOError:
-        print("WARNING: cannot chmod %s" % (base_file,))
-
 def do_edit_file(in_file, out_file, from_str, to_str):
     in_content = open(in_file).read()
     out_content = in_content.replace(from_str, to_str)
     open(out_file, 'w').write(out_content)
     
-class install(_install):
-    def run(self):
-        _install.run(self)
-        do_edit_file('bin/indicator-armadito.in', 'bin/indicator-armadito', '@PREFIX@', self.prefix)
-        do_install_file('bin/indicator-armadito', self.prefix + '/bin')
-        print("Done!")
-
 def read(filename):
     return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
@@ -67,8 +42,7 @@ setup(
         'icons/scalable/indicator-armadito-desactive.svg',
         'icons/scalable/indicator-armadito-down.svg',
         'icons/scalable/indicator-armadito-missing.svg',
-        'icons/scalable/indicator-armadito.svg'])],
-#    cmdclass = {"install": install}
+        'icons/scalable/indicator-armadito.svg'])]
 )
 
 #
